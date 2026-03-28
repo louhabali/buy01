a2206475c001242e7715c5fd1fc741409334b98a
louhabali <louhabali@gmail.com>
2026-03-28 11:59:50 +0100

    ft : adding 4 separate springboot apps to work with microservices logic not monolith

---
+++ user_service/src/test/java/buy01/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package buy01.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
