189d8556fe4e55d401e776855b442f42a2bdc5f1
louhabali <louhabali@gmail.com>
2026-07-10 19:09:31 +0100

    redis config , ft : the feature has been successfully added

--- user_service/src/main/java/buy01/user_service/dto/ProfileResponse.java
+++ user_service/src/main/java/buy01/user_service/dto/ProfileResponse.java
@@ -3,8 +3,10 @@ package buy01.user_service.dto;
 import buy01.user_service.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ProfileResponse {
 
