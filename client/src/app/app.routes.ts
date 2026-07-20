74ec75a273c01d76501fe8d18c77553dfd00bf80
louhabali <louhabali@gmail.com>
2026-07-20 18:14:08 +0100

    ft : adding guards for sellers and not logged guys + checks in interceptors

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,23 +4,29 @@ import { RegisterComponent } from './auth/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { authGuard } from '../guards/auth.guard';
+import { guestGuard } from '../guards/guest.guard';
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate : [authGuard]
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate : [guestGuard]
   },
   {
     path: 'register',
-    component: RegisterComponent
+    component: RegisterComponent,
+    canActivate : [guestGuard]
   },
 
   {
     path: 'profile',
-    component: ProfileComponent
+    component: ProfileComponent ,
+    canActivate : [authGuard]
   },
   { path: 'unauthorized', component: ErrorComponent, data: { code: '401' } },
   { path: 'forbidden', component: ErrorComponent, data: { code: '403' } },
